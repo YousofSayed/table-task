6b5e7391c6eba2a65991dc4ae0735e64e0da881d
yousofsayed <yousef.sayed1231@gmail.com>
2024-09-16 12:21:55 -0700

    cm-1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import {makeAppResponsive} from './helpers/cocktail.ts'
 import './index.css'
 
-makeAppResponsive(`#root`);
+const root : HTMLElement = document.querySelector(`#root`)!;
+  root.style.height = `${window.innerHeight}px`;
+  window.addEventListener(
+    "resize",
+    () => (root.style.height = `${window.innerHeight}px`)
+  );
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
